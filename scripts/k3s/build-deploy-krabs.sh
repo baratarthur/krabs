e9aecfd67479a8ba66faa87fd263688038cfc208
Arthur Barata <arthurpbarata@gmail.com>
2026-03-18 23:19:24 -0300

    remove rbac from main script

--- scripts/k3s/build-deploy-krabs.sh
+++ scripts/k3s/build-deploy-krabs.sh
@@ -6,7 +6,6 @@ source scripts/k3s/load-cronjob.sh watch
 source scripts/k3s/load-krabs-coordinator.sh
 
 # deploy
-sudo k3s kubectl apply -f krabs-coordinator/krabs-coordinator-rbac.yaml
 sudo k3s kubectl apply -f krabs-coordinator/krabs-postgres.yaml
 sleep 2m
 sudo k3s kubectl apply -f krabs-coordinator/krabs-coordinator-deployment.yaml
