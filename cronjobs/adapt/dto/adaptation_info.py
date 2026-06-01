53f34f27799a9f2e5212a1d45c0298e5f0ae23cc
Arthur Barata <a221738@dac.unicamp.br>
2026-06-01 16:13:10 -0300

    more steps in adaptation

---
+++ cronjobs/adapt/dto/adaptation_info.py
@@ -0,0 +1,14 @@
+import os
+
+TARGET_APP = os.getenv('TARGET', 'social-media-app')
+
+class AdaptationInfo:
+    def __init__(self, app_info, cluster_info):
+        self.initial_port = 30300
+        self.current_num_replicas = int(app_info['num_replicas'])
+        self.initial_name = f'dana-remote-{TARGET_APP}'
+        self.remotes = []
+        self.available_cores = int(cluster_info['cores']) - len(cluster_info['components']) - len(cluster_info['applications'])
+
+    def add_remote(self, ip_address, app_port):
+        self.remotes.append({"name": ip_address, "port": app_port})
--- cronjobs/adapt/script.py
+++ cronjobs/adapt/script.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from dto.adaptation_info import AdaptationInfo
 
 TARGET_APP = os.getenv('TARGET', 'social-media-app')
 CLUSTER_NAME = os.getenv('CLUSTER_NAME', 'unknown')
@@ -22,7 +23,7 @@ def fetch_data(url, params):
     except requests.exceptions.RequestException as e:
         print(f"Erro de conexão ao buscar dados ({params.get('type')}): {e}")
         return None
-    
+
 def create_data(url, body):
     try:
         response = requests.post(url, json=body, verify=False, timeout=45)
@@ -61,15 +62,15 @@ def main():
         print("Não foi possível obter as informações da aplicação. Encerrando o processo de adaptação.")
         return
     
-    app_latency = fetch_data(LATENCY_RETREIVE_URL(cluster_info['ip_address'], app_info['port']), params={})
+    app_meta = fetch_data(LATENCY_RETREIVE_URL(cluster_info['ip_address'], app_info['port']), params={})
 
     print(f"Informações da aplicação: {app_info}")
     print(f"Informações do cluster: {cluster_info}")
-    print(f"Latência atual da aplicação: {app_latency['latency']} ms")
+    print(f"Latência atual da aplicação: {app_meta['latency']} ms")
 
     try:
         last_latency = round(float(app_info['last_latency_check']), 2)
-        current_latency = round(float(app_latency['latency']), 2)
+        current_latency = round(float(app_meta['latency']), 2)
 
         if last_latency == current_latency:
             print("INFO: Stable latency.")
@@ -85,48 +86,47 @@ def main():
                 update_data(APP_INFO_URL, body={"num_replicas": app_info['num_replicas'], "config": app_info['config'],
                                 "last_latency_counter": counter, "last_latency_check": current_latency})
             return
+        
+        adaptation_info = AdaptationInfo(app_info, cluster_info)
 
-        initial_port = 30300
-        current_num_replicas = int(app_info['num_replicas'])
-        initial_name = f'dana-remote-{TARGET_APP}'
-        remotes = []
-
+        # verify adaptation need and set parameters replicas and configuration
         if current_latency > TRESHOLD_LATENCY:
             print("INFO: latency increasing.")
-            num_replicas = 2 if current_num_replicas < 2 else current_num_replicas + 1
+            num_replicas = 2 if adaptation_info.current_num_replicas < 2 else adaptation_info.current_num_replicas + 1
             CONFIG = 4
         else:
             print("INFO: latency decreasing.")
-            num_replicas = 0 if current_num_replicas < 3 else current_num_replicas - 1
+            num_replicas = 0 if adaptation_info.current_num_replicas < 3 else adaptation_info.current_num_replicas - 1
             CONFIG = 0 if num_replicas < 3 else 4
 
-        
+        # adaptation url and request the creation of a namespace to handle application remotes
         adaptation_url = f"http://{cluster_info['ip_address']}:{app_info['port']}/adapt/{CONFIG}"
-        body = {"num_replicas": num_replicas, "config": CONFIG, "last_latency_counter": 0, "last_latency_check": current_latency}
-        namespace = f"{initial_name}-ns-{num_replicas}-replicas" # different namespaces for each number of replicas to avoid conflicts
+        
+        namespace = f"{adaptation_info.initial_name}-replicas"
         create_data(f'{POD_CREATOR_URL(cluster_info["ip_address"])}/namespaces', body={"name": namespace})
         print(f"Namespace created: {namespace}")
 
         for i in range(num_replicas):
             new_remote = {
-                "pod_name": f'{initial_name}-{i}',
+                "pod_name": f'{adaptation_info.initial_name}-{i}',
                 "namespace": namespace,
                 "image_name": "my.private-registry.lan:5000/dana-remote:latest",
-                "app_port": initial_port + (10*num_replicas) + i, # unique port for each replica according to number of total replicas to avoid conflicts
+                "app_port": adaptation_info.initial_port + i, # unique port for each replica according to number of total replicas to avoid conflicts
             }
             create_data(f'{POD_CREATOR_URL(cluster_info["ip_address"])}/create-pod', body=new_remote)
             print(f"Request to create pod sent: {new_remote}")
-            remotes.append({"name": cluster_info['ip_address'], "port": new_remote['app_port']})
+            adaptation_info.add_remote(cluster_info['ip_address'], new_remote['app_port'])
 
-        print(f"Remotes created: {remotes}")
-        create_data(adaptation_url, body=remotes)
+        print(f"Remotes created: {adaptation_info.remotes}")
+        create_data(adaptation_url, body=adaptation_info.remotes)
         print(f"App adapted to configuration {CONFIG} with {num_replicas} replicas.")
         
-        print(f"App update body = {body}")
-        update_data(APP_INFO_URL, body=body)
+        new_app_information = {"num_replicas": num_replicas, "config": CONFIG, "last_latency_counter": 0, "last_latency_check": current_latency}
+        print(f"App update body = {new_app_information}")
+        update_data(APP_INFO_URL, body=new_app_information)
 
         print("Delete old namespaces and pods to free cpu capacity in cluster.")
-        old_namespace = f"{initial_name}-ns-{current_num_replicas}-replicas" # different namespaces for each number of replicas to avoid conflicts
+        old_namespace = f"{adaptation_info.initial_name}-ns-{adaptation_info.current_num_replicas}-replicas" # different namespaces for each number of replicas to avoid conflicts
         delete_data(f'{POD_CREATOR_URL(cluster_info["ip_address"])}/namespaces/{old_namespace}')
 
         print("Adaptation complete.")
